6665844394170d65586dba1462be91c7f55cafc4
KyumMulani7 <kyummulani7@gmail.com>
2022-05-02 08:11:03 +0530

    Heroku fix

--- server.js
+++ server.js
@@ -79,31 +79,31 @@ io.on("connection", (socket) => {
 });
 
 //Compiler logic
-app.post("/run", async (req, res) => {
-  const { language = "cpp", code } = req.body;
+// app.post("/run", async (req, res) => {
+//   const { language = "cpp", code } = req.body;
 
-  if (code === undefined) {
-    return res.status(400).json({ success: false, error: "Empty code body!" });
-  }
-  const filepath = await generateFile(language, code);
-  let output;
-  if (language === "py") {
-    output = await executePy(filepath);
-  } else if (language === "js") {
-    output = await executeNode(filepath);
-  }
+//   if (code === undefined) {
+//     return res.status(400).json({ success: false, error: "Empty code body!" });
+//   }
+//   const filepath = await generateFile(language, code);
+//   let output;
+//   if (language === "py") {
+//     output = await executePy(filepath);
+//   } else if (language === "js") {
+//     output = await executeNode(filepath);
+//   }
 
-  const deleteFile = filepath;
-  if (fs.existsSync(deleteFile)) {
-    fs.unlink(deleteFile, (err) => {
-      if (err) {
-        console.log(err);
-      }
-      console.log("deleted code file");
-    });
-  }
-  return res.json({ filepath, output });
-});
+//   const deleteFile = filepath;
+//   if (fs.existsSync(deleteFile)) {
+//     fs.unlink(deleteFile, (err) => {
+//       if (err) {
+//         console.log(err);
+//       }
+//       console.log("deleted code file");
+//     });
+//   }
+//   return res.json({ filepath, output });
+// });
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
