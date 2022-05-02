c548ffcf3a5f97feebf1dcb7c159bf5708f44859
KyumMulani7 <kyummulani7@gmail.com>
2022-05-02 06:35:10 +0530

    Added requirement txt file

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = new Server(server);
 
-// app.use(express.static("build"));
-// app.use((req, res, next) => {
-//   res.sendFile(path.join(__dirname, "build", "index.html"));
-// });
+app.use(express.static("build"));
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 app.use(express.static(__dirname));
 
 // app.get("/*", function (req, res) {
