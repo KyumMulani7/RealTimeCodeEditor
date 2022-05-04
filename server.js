70c5be09b374070d9a79b12f87ab202653909bc6
KyumMulani7 <kyummulani7@gmail.com>
2022-05-04 11:03:43 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = new Server(server);
 
-// app.use(express.static("build"));
+app.use(express.static("build"));
 // app.use((req, res, next) => {
 //   res.sendFile(path.join(__dirname, "build", "index.html"));
 // });
