e7ab77ac6297dc80c1af6657c47bbd011a88ac76
Kyum Mulani <68967484+KyumMulani7@users.noreply.github.com>
2023-07-17 02:28:03 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Grow together by Real time collab with your code/i);
   expect(linkElement).toBeInTheDocument();
 });
